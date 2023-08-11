c913bd1a53925bc60afc5d745ee3da9435817d5f
Ravil <hi-rav@yandex.com>
2023-08-11 14:16:05 +0300

    Started working on Lesson 5 HW

--- src/main/java/geekbrains/hw/lesson5/Task1.java
+++ src/main/java/geekbrains/hw/lesson5/Task1.java
@@ -1,8 +1,6 @@
-package geekbrains;
+package geekbrains.hw.lesson5;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Task1 {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
